44ba128ca3bb901ed4129388f55ac6184a8fdf0b
jforbush-revealiency <jforbush@revealiency.com>
2026-03-06 08:45:20 -0700

    Update vehicle.rb

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,8 +2,17 @@ class Vehicle < ApplicationRecord
   belongs_to :engine_config
   belongs_to :location
   has_many :inputs
-  validates :code, presence: true
-  validates_uniqueness_of :code, scope: :location_id
+
+  validates :folder_code, presence: true
+  validates_uniqueness_of :folder_code, scope: :location_id
+
+  # Auto-populate code from folder_code before saving
+  before_save :set_code_from_folder_code
+
+  def set_code_from_folder_code
+    self.code ||= self.folder_code
+  end
+
   def previous_engine_hours(current_input)
     previous_input = self.inputs.where("id < ?", current_input.id).order("id DESC").limit(1).first
     engine_hours = 0.0
@@ -12,30 +21,38 @@ class Vehicle < ApplicationRecord
     end
     engine_hours
   end
-  def company_code 
+
+  def company_code
     "#{location.company.code}"
   end
-  def location_code 
+
+  def location_code
     "#{location.code}"
   end
-  def manufacturer_code 
+
+  def manufacturer_code
     "#{engine_config.engine.manufacturer.code}"
   end
-  def engine_code 
+
+  def engine_code
     "#{engine_config.engine.code}"
   end
-  def engine_config_code 
+
+  def engine_config_code
     "#{engine_config.code}"
   end
-  def drive_type 
+
+  def drive_type
     "#{engine_config.engine.drive_type.code}"
   end
-  def is_single_stack 
+
+  def is_single_stack
     "#{engine_config.engine.is_single_stack}"
   end
+
   def as_json(options={})
-    super(methods: [:company_code, :location_code, :manufacturer_code, :engine_code, 
-                    :engine_config_code, :drive_type, :is_single_stack, :folder_code, 
+    super(methods: [:company_code, :location_code, :manufacturer_code, :engine_code,
+                    :engine_config_code, :drive_type, :is_single_stack, :folder_code,
                     :estimated_annual_vehicle_hours, :telematic])
   end
 end
