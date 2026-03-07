a9d9410db4f6f4c1e26db04a30d147f14cb1434b
jforbush-revealiency <jforbush@revealiency.com>
2026-03-07 10:41:29 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,35 @@ EngineConfig.find_each do |config|
   end
 end
 Input.where(test_type: nil).update_all(test_type: 'manual')
+
 puts "Linking inputs to vehicles..."
 linked = 0
-missing = 0
 Input.where(vehicle_id: nil).find_each do |input|
   next if input.vehicle_code.blank?
   v = Vehicle.find_by(code: input.vehicle_code)
   if v
     input.update_column(:vehicle_id, v.id)
     linked += 1
-  else
-    missing += 1
   end
 end
-puts "Linked: #{linked}, Missing: #{missing}"
+puts "Linked: #{linked}"
+
 puts "Updating statuses..."
+warning_codes = %w[warning Warning high High]
+caution_codes = %w[low Low caution Caution marginal]
+
 Vehicle.find_each do |vehicle|
   inp = Input.where(vehicle_id: vehicle.id).order(submitted: :desc).first
   next unless inp
   out = Output.find_by(input_id: inp.id)
   next unless out
-  s = out.overall_status == 'ok' ? 'in_spec' : out.overall_status == 'caution' ? 'marginal' : 'critical'
-  vehicle.update_column(:last_diagnostic_status, s) rescue nil
-  vehicle.update_column(:last_test_date, inp.submitted) rescue nil
-  puts "  #{vehicle.code} => #{s}"
-end
-puts "Done!"
+
+  all_codes = [
+    out.co2_percent_left_bank_code,
+    out.co2_percent_right_bank_code,
+    out.co_left_bank_code,
+    out.co_right_bank_code,
+    out.nox_left_bank_code,
+    out.nox_right_bank_code,
+    out.bank_balance_check_co2_percent_code,
+    out.bank_bala
