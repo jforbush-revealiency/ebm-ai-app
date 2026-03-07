802d24f3e3f7479637d3a7d84cfea187813e8933
jforbush-revealiency <jforbush@revealiency.com>
2026-03-06 22:05:56 -0700

    Update 20260307000000_fix_telematic_test_types.rb

--- db/migrate/20260307000000_fix_telematic_test_types.rb
+++ db/migrate/20260307000000_fix_telematic_test_types.rb
@@ -1,16 +1,18 @@
 class FixTelematicTestTypes < ActiveRecord::Migration[7.1]
   def up
-    # Set all auto-generated inputs to manual first
-    Input.where(auto_generated: true).update_all(test_type: 'manual')
-    
-    # Then re-set only the Redmond HT4 telematic records back to telematic
-    # These are identified by their vehicle codes starting with 'Testht'
-    Vehicle.where("LOWER(folder_code) LIKE 'testht%'").each do |v|
-      Input.where(vehicle_code: v.folder_code).update_all(test_type: 'telematic')
+    # Add the column if it doesn't exist
+    unless column_exists?(:inputs, :test_type)
+      add_column :inputs, :test_type, :string, default: 'manual'
     end
+
+    # Set all auto-generated inputs to manual
+    execute "UPDATE inputs SET test_type = 'manual' WHERE auto_generated = true"
+
+    # Set Redmond HT4 telematic records back to telematic
+    execute "UPDATE inputs SET test_type = 'telematic' WHERE vehicle_code IN (SELECT folder_code FROM vehicles WHERE LOWER(folder_code) LIKE 'testht%')"
   end
 
   def down
-    Input.where(auto_generated: true).update_all(test_type: nil)
+    remove_column :inputs, :test_type if column_exists?(:inputs, :test_type)
   end
 end
