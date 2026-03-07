fb56151eca5ceb81401b560f0b03084e6f5245da
jforbush-revealiency <jforbush@revealiency.com>
2026-03-06 22:17:24 -0700

    Update base_controller.rb

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,5 @@
 module Api
-  class BaseController < ApplicationController
+  class BaseController < ::ApplicationController
     skip_before_action :verify_authenticity_token
     before_action :set_default_format
 
