7c0e322bab7857ee3a0772f516fb723654340301
jforbush-revealiency <jforbush@revealiency.com>
2026-03-07 09:05:55 -0700

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,19 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    allowed = [
-      'http://localhost:3001',
-      'http://localhost:3000',
-      'https://reportgarden-pro.lovable.app',
-      'https://*.lovable.app',
-      'https://*.lovableproject.com',
-      ENV['FRONTEND_URL']
-    ].compact
-    origins(*allowed)
+    origins '*'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization'],
-      credentials: true,
-      max_age: 86400
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
