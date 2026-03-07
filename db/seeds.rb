c14ebd71247060c4ce8e4618c70e36be4057c5cc
jforbush-revealiency <jforbush@revealiency.com>
2026-03-07 10:43:00 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,28 @@
 puts "Seeding baselines..."
-EngineConfig.find_each do |config|
-  if config.co2_percent.nil? || config.co2_percent == 0
-    config.update(co2_percent: 12.5)
-    puts "  Updated: #{config.code}"
-  end
+EngineConfig.find_each do |c|
+  c.update(co2_percent: 12.5) if c.co2_percent.nil? || c.co2_percent == 0
 end
 Input.where(test_type: nil).update_all(test_type: 'manual')
-
-puts "Linking inputs to vehicles..."
+puts "Linking inputs..."
 linked = 0
-Input.where(vehicle_id: nil).find_each do |input|
-  next if input.vehicle_code.blank?
-  v = Vehicle.find_by(code: input.vehicle_code)
-  if v
-    input.update_column(:vehicle_id, v.id)
-    linked += 1
-  end
+Input.where(vehicle_id: nil).find_each do |i|
+  next if i.vehicle_code.blank?
+  v = Vehicle.find_by(code: i.vehicle_code)
+  next unless v
+  i.update_column(:vehicle_id, v.id)
+  linked += 1
 end
 puts "Linked: #{linked}"
-
 puts "Updating statuses..."
-warning_codes = %w[warning Warning high High]
-caution_codes = %w[low Low caution Caution marginal]
-
-Vehicle.find_each do |vehicle|
-  inp = Input.where(vehicle_id: vehicle.id).order(submitted: :desc).first
-  next unless inp
-  out = Output.find_by(input_id: inp.id)
-  next unless out
-
-  all_codes = [
-    out.co2_percent_left_bank_code,
-    out.co2_percent_right_bank_code,
-    out.co_left_bank_code,
-    out.co_right_bank_code,
-    out.nox_left_bank_code,
-    out.nox_right_bank_code,
-    out.bank_balance_check_co2_percent_code,
-    out.bank_bala
+Vehicle.find_each do |v|
+  i = Input.where(vehicle_id: v.id).order(submitted: :desc).first
+  next unless i
+  o = Output.find_by(input_id: i.id)
+  next unless o
+  codes = "#{o.co2_percent_left_bank_code} #{o.co2_percent_right_bank_code} #{o.co_left_bank_code} #{o.co_right_bank_code} #{o.nox_left_bank_code} #{o.nox_right_bank_code}"
+  s = codes.match?(/warning|Warning|high|High/) ? 'critical' : codes.match?(/low|Low|caution|Caution/) ? 'marginal' : 'in_spec'
+  v.update_column(:last_diagnostic_status, s) rescue nil
+  v.update_column(:last_test_date, i.submitted) rescue nil
+  puts "  #{v.code} => #{s}"
+end
+puts "Done!"
