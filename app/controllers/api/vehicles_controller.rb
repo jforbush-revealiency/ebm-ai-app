63cdaa891e43e07641390364b7bfaf845080e51a
jforbush-revealiency <jforbush@revealiency.com>
2026-03-07 11:00:30 -0700

    Update vehicles_controller.rb

--- app/controllers/api/vehicles_controller.rb
+++ app/controllers/api/vehicles_controller.rb
@@ -7,10 +7,10 @@ class Api::VehiclesController < ApplicationController
         code: v.code,
         description: v.description,
         emission_test_count: Input.where(vehicle_id: v.id).count,
-        last_diagnostic_status: v.last_diagnostic_status,
-        last_test_date: v.last_test_date,
-        company: v.company&.name,
-        location: v.location&.description
+        last_diagnostic_status: v.try(:last_diagnostic_status),
+        last_test_date: v.try(:last_test_date),
+        company: v.try(:company_code),
+        location: v.try(:location_code)
       }
     }
   end
@@ -21,12 +21,15 @@ class Api::VehiclesController < ApplicationController
       id: vehicle.id,
       code: vehicle.code,
       description: vehicle.description,
-      last_diagnostic_status: vehicle.last_diagnostic_status,
-      last_test_date: vehicle.last_test_date,
-      company: vehicle.company&.name,
-      inputs: Input.where(vehicle_id: vehicle.id).order(submitted: :desc).limit(10).map { |i|
-        { id: i.id, submitted: i.submitted, engine_hours: i.engine_hours }
-      }
+      last_diagnostic_status: vehicle.try(:last_diagnostic_status),
+      last_test_date: vehicle.try(:last_test_date),
+      company: vehicle.try(:company_code),
+      inputs: Input.where(vehicle_id: vehicle.id)
+                   .order(submitted: :desc)
+                   .limit(10)
+                   .map { |i|
+                     { id: i.id, submitted: i.submitted, engine_hours: i.engine_hours }
+                   }
     }
   end
 end
