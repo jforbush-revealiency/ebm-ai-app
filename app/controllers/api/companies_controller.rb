b4338d3799dea39c04f8b0b71e08facae9a35cc4
jforbush-revealiency <jforbush@revealiency.com>
2026-03-08 11:03:14 -0600

    Create companies_controller.rb

---
+++ app/controllers/api/companies_controller.rb
@@ -0,0 +1,46 @@
+module Api
+  class CompaniesController < ApplicationController
+    def index
+      companies = Company.includes(:locations).all.order(:name)
+      render json: companies.map { |company|
+        {
+          id: company.id,
+          code: company.code,
+          name: company.description || company.code,
+          description: company.description,
+          active: company.active,
+          locations: company.locations.order(:name).map { |loc|
+            {
+              id: loc.id,
+              code: loc.code,
+              description: loc.description,
+              active: loc.active,
+              company_id: loc.company_id
+            }
+          }
+        }
+      }
+    end
+
+    def update
+      company = Company.find(params[:id])
+      if company.update(company_params)
+        render json: {
+          id: company.id,
+          code: company.code,
+          name: company.description || company.code,
+          description: company.description,
+          active: company.active
+        }
+      else
+        render json: { errors: company.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def company_params
+      params.require(:company).permit(:description, :active)
+    end
+  end
+end
