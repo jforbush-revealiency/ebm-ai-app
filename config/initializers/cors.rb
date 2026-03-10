2097f46df840e22c1285bdfcf000a682117e17b2
jforbush-revealiency <jforbush@revealiency.com>
2026-03-10 08:21:59 -0600

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,9 +3,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'localhost:3000',
             'localhost:8080',
             /\.lovableproject\.com$/,
+            /\.lovable\.dev$/,
+            'lovable.dev',
             'dieselenginetech.lovable.app',
             'reportgarden-pro.lovable.app'
-
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
