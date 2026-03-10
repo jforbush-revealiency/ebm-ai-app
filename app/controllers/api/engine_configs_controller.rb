3594fd4f7c8b68cab52c2b81a6ecb4192ba113c5
jforbush-revealiency <jforbush@revealiency.com>
2026-03-09 19:32:07 -0600

    Create engine_configs_controller.rb

---
+++ app/controllers/api/engine_configs_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  class EngineConfigsController < ApplicationController
+    def update
+      config = EngineConfig.find(params[:id])
+      if config.update(params.require(:engine_config).permit!)
+        render json: config.as_json
+      else
+        render json: { errors: config.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  end
+end
