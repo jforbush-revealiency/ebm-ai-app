e1d3771561c8bc6bd34856044fc50fc31a4e9eba
jforbush-revealiency <jforbush@revealiency.com>
2026-03-09 23:01:34 -0600

    Update debug_controller.rb

--- app/controllers/api/debug_controller.rb
+++ app/controllers/api/debug_controller.rb
@@ -1,22 +1,21 @@
 class Api::DebugController < ApplicationController
   def engine_configs
-    configs = EngineConfig.all.map do |ec|
+    configs = EngineConfig.includes(:engine).map do |ec|
+      engine_name = ec.engine&.description || ec.engine&.code || ec.code || "Config ##{ec.id}"
       {
-        id: ec.id,
-        engine: ec[:engine],
-        co2_percent: ec.co2_percent,
-        co: ec.co,
-        nox: ec.nox,
-        rated_rpm: ec.rated_rpm,
-        rated_hp: ec.rated_hp,
-        operating_rpm: ec[:operating_rpm],
-        target_load: ec[:target_load]
+        id:           ec.id,
+        code:         ec.code,
+        engine_name:  engine_name,
+        engine:       engine_name,
+        co2_percent:  ec.co2_percent,
+        co:           ec.co,
+        nox:          ec.nox,
+        rated_rpm:    ec.rated_rpm,
+        rated_hp:     ec.rated_hp,
+        operating_rpm: ec.operating_rpm,
+        target_load:  ec.target_load
       }
     end
     render json: configs
-  end
-
-  def parameters
-    render json: Parameter.all.map { |p| { id: p.id, code: p.code, value: p.value } }
   end
 end
