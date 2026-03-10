f86c350602dd2554796901b531bfa4f2f76bbcfd
jforbush-revealiency <jforbush@revealiency.com>
2026-03-09 21:43:35 -0600

    Update engine_configs_controller.rb

--- app/controllers/api/engine_configs_controller.rb
+++ app/controllers/api/engine_configs_controller.rb
@@ -2,7 +2,11 @@ module Api
   class EngineConfigsController < BaseController
     def update
       config = EngineConfig.find(params[:id])
-      if config.update(params.require(:engine_config).permit!)
+      allowed = params.require(:engine_config).permit(
+        :engine, :co2_percent, :co, :nox, :rated_rpm, :rated_hp,
+        :operating_rpm, :target_load
+      )
+      if config.update(allowed)
         render json: config.as_json
       else
         render json: { errors: config.errors.full_messages }, status: :unprocessable_entity
